43759f20770347721985e1cbf2819c4cabdd4aa0
Preslav Aleksandrov <preslavaleksandrov@pop-os.localdomain>
2020-07-05 21:28:02 +0100

    Commit

--- Gauss-Newton/GN.py
+++ Gauss-Newton/GN.py
@@ -1,9 +1,11 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from decimal import *
 
 inputList = []
 outputList = []
-
+delta = 0.00000001
+consts = [1, 90]
+np.seterr(all='ignore')
 # Data reader function
 # takes data from location set @ variable and puts it into memory
 
@@ -25,6 +27,18 @@ def dataReader(location):
                     outputList.append(float(a))
                     a = ""
                     first = True
+    reader.close()
+
+
+def dataWriter(location, A, B):
+    with open(location, 'w+') as writer:
+        writer.write("J\n")
+        for i in A:
+            writer.write(np.array_str(i)[1:-1] + '\n')
+        writer.write("R\n")
+        for i in B:
+            writer.write(str(i) + '\n')
+    writer.close()
 
 # returns the square sum of a list
 
@@ -35,12 +49,41 @@ def sSquares(list):
 # returns the oputput of the specified function
 
 
-def f(a, b, x):
-    return a*np.exp(b*x)
+def f(x, params):
+    return params[0]*np.exp(params[1]*x)
 
 
-def residual():
-    r
+def partial_derivative(x, index):
+    n_consts = consts.copy()
+    n_consts[index] = n_consts[index] - (delta/2)
+    f1 = f(x, n_consts)
+    n_consts[index] = n_consts[index] + delta
+    f2 = f(x, n_consts)
+    return (f2-f1)/delta
+
+
+def Jacobian():
+    J = []
+    for i in inputList:
+        j1 = []
+        for j in range(len(consts)):
+            j1.append(partial_derivative(i, j))
+        J.append(j1)
+    return np.array(J, dtype='d')
+
+
+def Residual():
+    R = []
+    for i in np.arange(len(outputList)):
+        R.append(outputList[i]-f(inputList[i], consts))
+    return np.array(R, dtype='d')
 
 
 dataReader('Gauss-Newton/data.raw')
+for i in range(10):
+    J = Jacobian()
+    R = Residual()
+    dataWriter('Gauss-Newton/output_small.raw', J, R)
+    c = np.matmul(np.matmul(np.linalg.inv(np.matmul(J.transpose(), J)), J.transpose()), R)
+    consts = consts + c
+    print(consts)
