6e1298f7b7a2db8ae4a742dfdb0208503ecb2538
Preslav Aleksandrov <preslavaleksandrov@pop-os.localdomain>
2020-08-29 14:21:48 +0100

    m

--- seedingTest.py
+++ seedingTest.py
@@ -1,9 +1,9 @@
-class Points
+class Points:
     a = []
     def __init__(self, scope):
         self.scope = scope
-    def generate():
-        return scope
+    def generate(self):
+        return self.scope
 
 p = Points(20)
 print(p.generate())
