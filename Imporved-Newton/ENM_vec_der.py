399ba5973e7a74c30f4d708903254da2eaf08caa
Preslav Aleksandrov <preslavaleksandrov@pop-os.localdomain>
2020-11-03 21:20:42 +0000

    com

--- Imporved-Newton/ENM_vec_der.py
+++ Imporved-Newton/ENM_vec_der.py
@@ -9,47 +9,49 @@ c = np.array([2, 5],dtype=float)
 
 def fun(x):
     #  x_{1}^{3}-3x_1x_2^2-1\\
-    return np.array([np.exp(x[0])-x[1], x[0]*x[1]-np.exp(x[0])])
-    #  2return x[0]**2-x[1]**2-9
+    #1return np.array([np.exp(x[0])-x[1], x[0]*x[1]-np.exp(x[0])])
+    return np.array([x[0]**2-x[1]**2-9, 2*x[0]*x[1]])
     #  return x[0]**3-3*x[0]*x[1]**2-1
+
+
+def grad_fun(x):
+    #return np.array([np.exp(x[0]), -1, x[1]-np.exp(x[0]), x[0]]).reshape(2, 2)
+    return np.array([2*x[0], -2*x[1], 2*x[1] ,2*x[0]]).reshape(2, 2)
 
 
 def P(c,x):
     a = x-c
     b = fun(x)-fun(c)
     d = fun(x)
-    #print("P out", x)
-    #print(np.matmul((d/b).reshape(2,-1), a.reshape(-1,2)))
     return np.matmul(a.reshape(2,-1),(d/b).reshape(-1,2)).T
 
 
-def getPartial(f, x):
-    global delta
-    """
-    dt = np.array([delta,-delta])
-    xx = np.array(np.meshgrid(x, dt)).T.reshape(-1,2)
-    xx = np.sum(xx, axis=1)
-    xx = np.split(xx,2)
-    xx = np.vstack((np.array(np.meshgrid(xx[0], x[1])).T.reshape(-1,2),np.array(np.meshgrid(x[0],xx[1])).T.reshape(-1,2)))
-    vals = np.apply_along_axis(f,1,xx)
-    print(xx)
-    """
-    vs = None
-    for i in range(2):
-        a = x.copy()
-        b = x.copy()
-        a[i] += delta
-        b[i] -= delta
-        ans = np.vstack((a,b))
-        vs = np.vstack((vs, ans)) if vs is not None else ans
-    vals = np.apply_along_axis(f,1,vs)
-    #print("Vals", ((vals[::2]-vals[1::2])/(2*delta)).reshape(2,-1))
-    return ((vals[::2]-vals[1::2])/(2*delta)).reshape(2,-1).T
+def getPartial(x):
+    global c
+    el1 = x-c
+    el2 = fun(x)
+    el3 = el2-fun(c)
+    gr = grad_fun(x).T
+
+    num2 = el2*gr
+    num1 = el3*gr
+    den = el3**2
+    ans = np.outer(el1, ((num1-num2)/den).T)
+    ans[::, :2:] = ans[::, :2:].T
+    ans[::, 2:4:] = ans[::, 2:4:].T
+
+    ans1 = np.outer(np.identity(2), el2/el3).T    
+    hold = ans1[1::, :2:].copy()
+    ans1[1::, :2:] = ans1[:1:, 2:4:]
+    ans1[:1:, 2:4:] = hold
+
+    ans = ans + ans1
+    return ans.T
 
 
 def check_root(x):
-    ans = np.array([1, 2.718])
-    #ans =0
+    #ans = np.array([1, 2.718])
+    ans =0
     global delta
     #for i in self.f1:   
     #print(np.linalg.norm(i(x)))
@@ -61,30 +63,31 @@ def check_root(x):
     #if ans <= delta:
     #    return True
     return False
-
-
 #  Iterate method
 #
 # convergance criteria
+
+
 def solve(x):
     global delta
     global c
     cnt = 0
     for i in range(10):
         cnt += 1
-        #print(getPartial(partial(P, c), x))
-        p = pinv(getPartial(partial(P, c), x)) 
+        p = pinv(getPartial(x)) 
         q = P(c, x).reshape(-1, 1)
         step = np.matmul(p, q)
         if np.linalg.norm(step) < delta:
             break
         x = x - step.flatten()
-
+    #print(x)
     if check_root(x):
         return x, cnt
     return None, cnt
 
 
+#print(getPartial(x))
+
+
 print(solve(x))
 print(datetime.now() - startTime)
-#print(getPartial(x))
