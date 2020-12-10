2c66f7ebb06e414b0d9b3b877aa224d2823fa85e
Helios113 <preslav.aleksandrov@protonmail.com>
2020-12-10 16:04:43 +0000

    rearrange

---
+++ Newtons/Multivariate/Extended/ENM_fea.py
@@ -0,0 +1,89 @@
+import numpy as np
+from numpy.linalg import pinv
+from functools import partial
+from datetime import datetime
+startTime = datetime.now()
+delta = 1e-6
+conDelta = 0.001414
+
+def P(c,x):
+    a = x-c
+    b = fun(x)-fun(c)
+    d = fun(x)
+    #c = np.divide(d, b, out=np.zeros_like(d), where=b!=0)
+    return np.matmul(a.reshape(2,-1), (d/b).reshape(-1,2)).T
+
+
+def getPartial(x):
+    global c
+    el1 = x-c
+    el2 = fun(x)
+    el3 = el2-fun(c)
+    gr = grad_fun(x).T
+
+    num2 = el2*gr
+    num1 = el3*gr
+    den = el3**2
+
+    #d = np.divide((num1-num2), den, out=np.zeros_like(num1), where=den!=0)
+    ans = np.outer(el1, ((num1-num2)/den).T)
+    ans[::, :2:] = ans[::, :2:].T
+    ans[::, 2:4:] = ans[::, 2:4:].T
+
+    #a = np.divide(el2, el3, out=np.zeros_like(el2), where=el3!=0)
+    ans1 = np.outer(np.identity(2), el2/el3).T    
+    hold = ans1[1::, :2:].copy()
+    ans1[1::, :2:] = ans1[:1:, 2:4:]
+    ans1[:1:, 2:4:] = hold
+
+    ans = ans + ans1
+    return ans.T
+
+
+def check_root(x):
+    #ans = np.array([1, 2.718])
+    ans = 0
+    if isinstance(ans, np.ndarray):
+        if np.linalg.norm(ans-x.flatten()) <= conDelta:
+            return True
+    else:
+        ans += np.linalg.norm(fun(x))
+        if ans <= conDelta:
+            return True
+    return False
+#  Iterate method
+#
+# convergance criteria
+
+
+def solve(x):
+    global delta
+    global c
+    ans = np.array([1, 2.718])
+    c = x[2:].copy()
+    x = x[:2].copy()
+    cnt = 0
+    rate = []
+    order = []
+    error = []
+    for i in range(100):
+        error.append(np.linalg.norm(x-ans))
+        if len(error) > 2:
+            order.append(np.log10(error[-S1]/error[-2])/np.log10(error[-2]/error[-3]))
+        if len(order) > 0:
+            rate.append(error[-1]/(error[-2]**order[-1]))
+        cnt += 1
+        q = P(c, x).reshape(-1, 1)
+        p = pinv(getPartial(x)) 
+        step = np.matmul(p, q)
+        if np.linalg.norm(step) < delta:
+            break
+        x = x - step.flatten()
+    #print(x)
+    
+    if check_root(x):
+        return np.round(x, 3).tolist(), cnt, order, rate
+    return None, cnt, order, rate
+
+
+
